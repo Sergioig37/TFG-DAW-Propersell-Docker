4d932af4a2109fef6f499e51b25b4265b15dc1a6
Sergioig37 <sergioig37@educastur.es>
2024-06-06 14:55:09 +0200

    Nuevo script sql

--- data.sql
+++ data.sql
@@ -1,55 +1,51 @@
+CREATE DATABASE IF NOT EXISTS `propersell`;
 
-DROP DATABSE IF EXISTS `propersell`;
 
-CREATE DATABASE IF NOT EXISTS`propersell`;
-USE `propersell`;
 
+CREATE TABLE `alerta` (
+  `id_alerta` bigint(20) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_alerta`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `usuario` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_usuario` bigint(20) NOT NULL AUTO_INCREMENT,
   `correo` varchar(255) DEFAULT NULL,
+  `habilitado` bit(1) NOT NULL,
   `nombre_real` varchar(255) DEFAULT NULL,
+  `numero_telefono` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `role` enum('ADMIN','USER') DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- propersell.propiedad definition
+CREATE TABLE `alerta_cliente` (
+  `id_usuario` bigint(20) NOT NULL,
+  `id_alerta` bigint(20) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_alerta`),
+  KEY `FKjsryp9mnjewnmhg3i70buyspd` (`id_alerta`),
+  CONSTRAINT `FKe4aqsfcb5o8gyy5r1fgu98ddn` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `FKjsryp9mnjewnmhg3i70buyspd` FOREIGN KEY (`id_alerta`) REFERENCES `alerta` (`id_alerta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE `propiedad` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `habilitado` bit(1) NOT NULL,
   `localizacion` varchar(255) DEFAULT NULL,
-  `precio` varchar(255) DEFAULT NULL,
+  `precio` bigint(20) DEFAULT NULL,
   `tipo` varchar(255) DEFAULT NULL,
   `fk_propietario` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK3x2p6yka37ar1huirrh3ac2va` (`fk_propietario`),
-  CONSTRAINT `FK3x2p6yka37ar1huirrh3ac2va` FOREIGN KEY (`fk_propietario`) REFERENCES `cliente` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FKgmgmdcfl9qgadb3ka6rqkkro` (`fk_propietario`),
+  CONSTRAINT `FKgmgmdcfl9qgadb3ka6rqkkro` FOREIGN KEY (`fk_propietario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 
 
-CREATE TABLE `agente_cliente` (
-  `agente_id` bigint(20) NOT NULL,
-  `cliente_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`agente_id`,`cliente_id`),
-  KEY `FKpu1db7fy01erlxvfqerhotos8` (`cliente_id`),
-  CONSTRAINT `FKpu1db7fy01erlxvfqerhotos8` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
-  CONSTRAINT `FKq4r9346fjaq5wpncjxtlkwhqa` FOREIGN KEY (`agente_id`) REFERENCES `agente` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO usuario (username, password, correo, nombre_real, role, habilitado)
+VALUES ('SergioAdmin', '$2a$12$f/yfMl5wct2bAIS5HTdU0eRZNoSDejBBk.8vekifKuNQ81EpAbBD.', 'sergiotur04@gmail.com', 'Sergio Iglesias García', 'ADMIN', true);
 
-
-INSERT INTO Inmobiliaria (nombre, numero_empleados, direccion)
-VALUES ('ProperSell', '50', 'Dirección de la Inmobiliaria');
-
-INSERT INTO Inmobiliaria (nombre, numero_empleados, direccion)
-VALUES ('Raggg', '5', 'Dirección de la Inmobiliaria');
-
-INSERT INTO Inmobiliaria (nombre, numero_empleados, direccion)
-VALUES ('UAUFIASDFAS', '50O', 'Dirección de la Inmobiliaria');
-
-
-INSERT INTO Usuario (username, password, correo, nombre_real, role)
-VALUES ('SergioAdmin', '$2a$12$C7vvFjpIfXlLdK5pES6BHecQZBY8FZoXKz99xWeKuzwBp0cUFfIOi', 'sergiotur04@gmail.com', 'Nombre Real', 'ADMIN');
